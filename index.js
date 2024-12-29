128535e009a03d97925691137e250a8b7ba6161b
Šárka F. <sarka.flidrova@email.cz>
2024-12-29 10:30:51 +0100

    Leave only button on the page.

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const calendarInput = document.querySelector('#calendar');
 const calendarBtn = document.querySelector('#calendar-btn');
 
-// let selectedDates = [];
-
-// Save a calendar instance to a variable
-const calendarInstance = flatpickr(calendarInput, {
+flatpickr(calendarBtn, {
   mode: 'range',
   dateFormat: 'd M Y',
-  //   onChange: (dates) => {
-  //     selectedDates = dates.map((date) => date.toISOString().split('T')[0]);
-  //   },
-  clickOpens: false,
-});
-
-// Open the calendar when the button is clicked
-calendarBtn.addEventListener('click', () => {
-  calendarInstance.open();
 });
